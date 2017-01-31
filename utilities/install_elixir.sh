6ce56d8b78bd7a72252eb5db441cb50764eed87d
Matthew B. Reisch <matt.reisch@gmail.com>
2017-01-31 11:08:39 -0500

    Added more spacemacs themes. Elixir install script expanded to include dependencies for linting in editors.

--- utilities/install_elixir.sh
+++ utilities/install_elixir.sh
@@ -5,3 +5,19 @@ sudo dpkg -i /tmp/erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install esl-erlang
 sudo apt-get install elixir
+
+echo "Installing bunt and credo for flychecking"
+echo "Install bunt ..."
+git clone https://github.com/rrrene/bunt /tmp/bunt
+cd /tmp/bunt
+mix archive.build
+mix archive.install
+echo "Installing credo..."
+git clone https://github.com/rrrene/credo /tmp/credo
+cd /tmp/credo
+mix deps.get
+mix archive.build
+mix archive.install
+
+
+
